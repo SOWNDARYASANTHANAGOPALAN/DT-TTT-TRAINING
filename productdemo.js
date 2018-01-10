a14bdce98924d2cf4bbe630930072c6c89dcc745
SOWNDARYASANTHANAGOPALAN <35100236+SOWNDARYASANTHANAGOPALAN@users.noreply.github.com>
2018-01-10 12:29:15 +0530

    2nd HTML TASK

---
+++ productdemo.js
@@ -0,0 +1,27 @@
+function calTotal(){
+	var pprice=document.getElementById("pprice").value;
+	var pquan=document.getElementById("pquan").value;
+	var total=pprice*pquan;
+	var country=document.getElementById("country").value;
+	var distotal=total;
+	if(total>10000)
+	{
+		if(country=="India")
+			distotal=total-(total*0.5);
+		else if(country=="US")
+			distotal=total-(total*0.05);
+		
+	}
+	var cname=document.getElementById("cname").value;
+	var pname=document.getElementById("pname").value;
+	
+	var w=window.open("","_self")
+	w.document.writeln("<html><head><title>Product Display Page</title></head><body>");
+	w.document.writeln("Customer name: " + cname + "<br>");
+	w.document.writeln("Product name: " + pname + "<br>");
+	w.document.writeln("Total amount before discount: " + total+"<br>");
+	w.document.writeln("Total amount after discount: " + distotal + "<br>");
+	w.document.writeln("</body></html>");
+	
+	
+}
